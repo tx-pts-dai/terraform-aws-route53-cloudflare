54456fe4de40da5fd7c91c672552e21b38fa0d0e
David B <38657416+Davidoutz@users.noreply.github.com>
2024-06-13 16:31:35 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,3 +23,24 @@ variable "tags" {
   type        = map(any)
   default     = {}
 }
+
+variable "cf_domain_name" {
+  description = "The domain name to delegate in Cloudflare"
+  type        = string
+}
+
+variable "cf_aws_route53_name_servers" {
+  description = "Route53 name servers"
+  type        = list(string)
+}
+
+variable "cf_account_id" {
+  description = "Cloudflare API token"
+  type        = string
+}
+
+variable "cf_cloudflare_delegation" {
+  description = "Whether to delegate DNS to Cloudflare"
+  type        = bool
+  default     = false
+}
