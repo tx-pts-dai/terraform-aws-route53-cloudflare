f98e930819d70c7d0f04bad2635e0cd0b6fef9de
Davidoutz <david.beauverd@tx.group>
2024-06-13 11:44:23 +0200

    ok

--- modules/cloudflare/variables.tf
+++ modules/cloudflare/variables.tf
@@ -7,18 +7,3 @@ variable "aws_route53_name_servers" {
   description = "Route53 name servers"
   type        = list(string)
 }
-
-variable "cloudflare_email" {
-  description = "Cloudflare account email"
-  type        = string
-}
-
-variable "cloudflare_api_token" {
-  description = "Cloudflare API token"
-  type        = string
-}
-
-variable "account_id" {
-  description = "Cloudflare API token"
-  type        = string
-}
